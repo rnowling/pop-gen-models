64a1e9aee1aa7952c98ccbe5d7317ef8461a7d14
RJ Nowling <rnowling@gmail.com>
2015-05-09 22:40:17 -0500

    Use exp, not log

--- bamova/plot_sampled_phis.py
+++ bamova/plot_sampled_phis.py
@@ -11,7 +11,7 @@ def estimate_distribution(samples, h=0.2, n_points=100):
 	samples = samples[:, np.newaxis]
 	kde.fit(samples)
 	xs = np.linspace(-1.0, 1.0, n_points)
-	ys = [np.log(kde.score([x])) for x in xs]
+	ys = [np.exp(kde.score([x])) for x in xs]
 	return xs, ys
 
 def plot_phis(plot_flname, subset):
