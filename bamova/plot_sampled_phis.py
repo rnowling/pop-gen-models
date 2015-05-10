8d193a6eb59028eb2c21203bc8543d34603f86bd
RJ Nowling <rnowling@gmail.com>
2015-05-09 22:27:41 -0500

    Add bamova phi plotting script

---
+++ bamova/plot_sampled_phis.py
@@ -0,0 +1,38 @@
+import sys
+
+import numpy as np
+from sklearn.neighbors.kde import KernelDensity
+import matplotlib
+from matplotlib import pyplot
+
+def estimate_distribution(matrix, locus, h=0.2, n_points=100):
+	kde = KernelDensity(bandwidth=h)
+	kde.fit(matrix[:, locus])
+	xs = np.linspace(-1.0, 1.0, n_points)
+	ys = np.log(kde.score(xs))
+	return xs, ys
+
+def plot_phis(plot_flname, subset):
+	plt.hold(True)
+	for loci in xrange(subset.shape[1]):
+		xs, ys = estimate_distribution(subset, loci)
+		plt.plot(xs, ys)
+	plt.xlim([-1.0, 1.0])
+	plt.xlabel("\phi", fontsize=16)
+	plt.ylabel("Frequency", fontsize=16)
+	plt.savefig(plot_flname, DPI=200)
+
+
+if __name__ == "__main__":
+	npy_flname = sys.argv[1]
+	start = int(sys.argv[2])
+	end = int(sys.argv[3])
+	plot_flname = sys.argv[4]
+
+	phi_values = np.load(npy_flname)
+	subset = phi_values[:, start:end]
+
+
+
+
+
