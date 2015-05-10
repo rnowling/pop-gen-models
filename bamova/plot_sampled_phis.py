f6a0cf5ee3d3de859b7ad57d9331077903656d99
RJ Nowling <rnowling@gmail.com>
2015-05-09 22:36:22 -0500

    Fix call to score

--- bamova/plot_sampled_phis.py
+++ bamova/plot_sampled_phis.py
@@ -9,8 +9,9 @@ from matplotlib import pyplot as plt
 def estimate_distribution(samples, h=0.2, n_points=100):
 	kde = KernelDensity(bandwidth=h)
 	kde.fit(samples.T)
+	print samples.T.shape
 	xs = np.linspace(-1.0, 1.0, n_points)
-	ys = [np.log(kde.score(x)) for x in xs]
+	ys = [np.log(kde.score([x])) for x in xs]
 	return xs, ys
 
 def plot_phis(plot_flname, subset):
