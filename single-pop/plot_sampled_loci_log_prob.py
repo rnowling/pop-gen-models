Fix normalization

single-pop/plot_sampled_loci_log_prob.py
@@ -14,8 +14,8 @@ def estimate_distribution(samples, h=0.1, n_points=100):
 	kde.fit(samples)
 	xs = np.linspace(min_xs, max_xs, n_points)
 	ys = np.exp(kde.score_samples(xs[:, np.newaxis]))
-	print xs.shape, ys.shape
-	return xs, ys, sum(ys)
+	print xs.shape, ys.shape, sum(ys)
+	return xs, ys
 
 def plot_log_probs(plot_flname, subset):
 	plt.hold(True)